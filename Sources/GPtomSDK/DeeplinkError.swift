65a1d864ffd8047baff281bb167e469f56e60ce3
Jan Švec <jansvec92@gmail.com>
2026-02-06 10:40:05 +0100

    add new deeplink error

--- Sources/GPtomSDK/DeeplinkError.swift
+++ Sources/GPtomSDK/DeeplinkError.swift
@@ -12,4 +12,5 @@ public enum DeeplinkError: String, Equatable, CaseIterable, Codable, Sendable {
     case merchantInfoMissing
     case failedTapToPay
     case failedToCloseBatch
+    case unsupportedTransactionOperationOrType
 }
